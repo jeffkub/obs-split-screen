c5e6070af9ad0f8c4e06a783a3fe3f208eaf42d5
Jeff Kubascik <jeffkub@gmail.com>
2021-08-04 21:04:54 -0400

    Renamed functions

--- split-screen.c
+++ split-screen.c
@@ -3,46 +3,46 @@
 #include "version.h"
 #include "util/platform.h"
 
-static const char *split_get_name(void *type_data)
+static const char *split_screen_get_name(void *type_data)
 {
 	UNUSED_PARAMETER(type_data);
 	return obs_module_text("SplitScreen");
 }
 
-static void *split_create(obs_data_t *settings, obs_source_t *source)
+static void *split_screen_create(obs_data_t *settings, obs_source_t *source)
 {
 	return NULL;
 }
 
-static void split_destroy(void *data)
+static void split_screen_destroy(void *data)
 {
 
 }
 
-static void split_update(void *data, obs_data_t *settings)
+static void split_screen_update(void *data, obs_data_t *settings)
 {
 
 }
 
-static void split_video_render(void *data, gs_effect_t *effect)
+static void split_screen_video_render(void *data, gs_effect_t *effect)
 {
 
 }
 
-static bool split_audio_render(void *data, uint64_t *ts_out,
+static bool split_screen_audio_render(void *data, uint64_t *ts_out,
 				  struct obs_source_audio_mix *audio_output,
 				  uint32_t mixers, size_t channels,
 				  size_t sample_rate)
 {
-
+	return false;
 }
 
-uint32_t split_get_width(void *data)
+uint32_t split_screen_get_width(void *data)
 {
 	return 0;
 }
 
-uint32_t split_get_height(void *data)
+uint32_t split_screen_get_height(void *data)
 {
 	return 0;
 }
@@ -52,14 +52,14 @@ static struct obs_source_info split_screen = {
 	.type = OBS_SOURCE_TYPE_INPUT,
 	.output_flags = OBS_OUTPUT_VIDEO | OBS_SOURCE_CUSTOM_DRAW |
 			OBS_SOURCE_COMPOSITE | OBS_SOURCE_DO_NOT_DUPLICATE,
-	.get_name = split_get_name,
-	.create = split_create,
-	.destroy = split_destroy,
-	.update = split_update,
-	.video_render = split_video_render,
-	.audio_render = split_audio_render,
-	.get_width = split_get_width,
-	.get_height = split_get_height,
+	.get_name = split_screen_get_name,
+	.create = split_screen_create,
+	.destroy = split_screen_destroy,
+	.update = split_screen_update,
+	.video_render = split_screen_video_render,
+	.audio_render = split_screen_audio_render,
+	.get_width = split_screen_get_width,
+	.get_height = split_screen_get_height,
 	.icon_type = OBS_ICON_TYPE_SLIDESHOW,
 };
 
