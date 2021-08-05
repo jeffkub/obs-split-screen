e05ddee4656530dbf7b0d2ca1d7687dcc65f960b
Jeff Kubascik <jeffkub@gmail.com>
2021-08-04 22:55:23 -0400

    Working on configuration

--- split-screen.c
+++ split-screen.c
@@ -3,20 +3,32 @@
 #include "version.h"
 #include "util/platform.h"
 
+struct split_screen {
+	obs_source_t *source;
+};
+
+static void split_screen_update(void *data, obs_data_t *settings);
+
 static const char *split_screen_get_name(void *type_data)
 {
-	UNUSED_PARAMETER(type_data);
 	return obs_module_text("SplitScreen");
 }
 
 static void *split_screen_create(obs_data_t *settings, obs_source_t *source)
 {
-	return NULL;
+	struct split_screen *context = bzalloc(sizeof(struct split_screen));
+	context->source = source;
+
+	split_screen_update(context, settings);
+
+	return context;
 }
 
 static void split_screen_destroy(void *data)
 {
+	struct split_screen *context = data;
 
+	bfree(context);
 }
 
 static void split_screen_update(void *data, obs_data_t *settings)
@@ -30,11 +42,37 @@ static void split_screen_video_render(void *data, gs_effect_t *effect)
 }
 
 static bool split_screen_audio_render(void *data, uint64_t *ts_out,
-				  struct obs_source_audio_mix *audio_output,
-				  uint32_t mixers, size_t channels,
-				  size_t sample_rate)
+				      struct obs_source_audio_mix *audio_output,
+				      uint32_t mixers, size_t channels,
+				      size_t sample_rate)
 {
 	return false;
+}
+
+static obs_properties_t *split_screen_properties(void *data)
+{
+	obs_properties_t *ppts = obs_properties_create();
+
+	obs_properties_add_editable_list(ppts, S_SOURCES,
+					 obs_module_text("Sources"),
+					 OBS_EDITABLE_LIST_TYPE_STRINGS, NULL,
+					 NULL);
+
+	obs_properties_t *layout_group = obs_properties_create();
+	obs_properties_add_int(layout_group, S_LAYOUT_ROWS,
+			       obs_module_text("Rows"), 0, 100, 1);
+	obs_properties_add_int(layout_group, S_LAYOUT_COLUMNS,
+			       obs_module_text("Columns"), 0, 100, 1);
+	obs_properties_add_group(ppts, S_LAYOUT_GROUP,
+				 obs_module_text("Layout"), OBS_GROUP_NORMAL,
+				 layout_group);
+
+	return ppts;
+}
+
+static void split_screen_defaults(obs_data_t *settings)
+{
+
 }
 
 uint32_t split_screen_get_width(void *data)
@@ -58,6 +96,8 @@ static struct obs_source_info split_screen = {
 	.update = split_screen_update,
 	.video_render = split_screen_video_render,
 	.audio_render = split_screen_audio_render,
+	.get_properties = split_screen_properties,
+	.get_defaults = split_screen_defaults,
 	.get_width = split_screen_get_width,
 	.get_height = split_screen_get_height,
 	.icon_type = OBS_ICON_TYPE_SLIDESHOW,
--- split-screen.h
+++ split-screen.h
@@ -1,1 +1,7 @@
 #pragma once
+
+#define S_SOURCES "sources"
+
+#define S_LAYOUT_GROUP "layout_group"
+#define S_LAYOUT_ROWS "layout_rows"
+#define S_LAYOUT_COLUMNS "layout_columns"
